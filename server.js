4515dbae57952880d3f95b7c9cf8593d35e4bcde
idir bfs <belfares.idir@gmail.com>
2023-02-01 19:47:03 +0100

    add front init

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("Its working");
 });
-const PORT = process.env.PORT || 3000;
-app.listen(3000, console.log(`App is live on port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`App is live on port ${PORT}`));
